375bb22e572c3d7ee0ead5307080e599ec550c08
Tresdni <kalanbrock@gmail.com>
2015-12-15 08:44:55 -0500

    It now converts to webp format for Chrome users.

--- js/uriah.js
+++ js/uriah.js
@@ -1,12 +1,39 @@
 $(function(){
 
+    function getBrowserInfo(){
+        var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if(/trident/i.test(M[1])){
+            tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+            return {name:'IE',version:(tem[1]||'')};
+            }
+        if(M[1]==='Chrome'){
+            tem=ua.match(/\bOPR\/(\d+)/)
+            if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+            }
+        M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+        return {
+          name: M[0],
+          version: M[1]
+        };
+     }
+
+     var browser = getBrowserInfo();
+
     function getBase64Image(img) {
             var canvas = document.createElement("canvas");
             canvas.width = img.naturalWidth;
             canvas.height = img.naturalHeight;
             var ctx = canvas.getContext("2d");
             ctx.drawImage(img, 0, 0);
-            var dataURL = canvas.toDataURL("image/png");
+
+            var dataURL;
+            if(browser.name == "Chrome"){
+                dataURL = canvas.toDataURL("image/webp");
+            }
+            else {
+                dataURL = canvas.toDataURL("image/png");
+            }
 
             return dataURL;
     }
