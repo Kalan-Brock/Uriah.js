1dd8b3cddbb1f1f9bcd52fa76343ada12497d754
Kalan Brock <kalanbrock@gmail.com>
2015-12-18 15:16:01 -0500

    Fixed some JS Hint errors.

--- js/uriah.js
+++ js/uriah.js
@@ -7,11 +7,11 @@ $(function(){
             return {name:'IE',version:(tem[1]||'')};
             }
         if(M[1]==='Chrome'){
-            tem=ua.match(/\bOPR\/(\d+)/)
-            if(tem!=null)   {return {name:'Opera', version:tem[1]};}
+            tem=ua.match(/\bOPR\/(\d+)/);
+            if(tem!==null)   {return {name:'Opera', version:tem[1]};}
             }
         M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-        if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+        if((tem=ua.match(/version\/(\d+)/i))!==null) {M.splice(1,1,tem[1]);}
         return {
           name: M[0],
           version: M[1]
@@ -59,5 +59,5 @@ $(function(){
         else {
             $(image).attr('src', uriah);
         }
-    })
+    });
 });
